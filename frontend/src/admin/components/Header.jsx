6895446f3e2435a270dbff50668354686c0b3a6e
divyeshmoraniya <hdivyesh62@gmail.com>
2025-06-16 18:52:12 +0530

    Update Header.jsx

--- frontend/src/admin/components/Header.jsx
+++ frontend/src/admin/components/Header.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { LogOut ,User } from "lucide-react";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import adminImage from '../../public/VAH_20241202_232229_0000_page-0001.jpg';
 
 const Header = ({ isOpen }) => {
   const location = useLocation();
@@ -82,7 +83,7 @@ const Header = ({ isOpen }) => {
           >
             {isAdmin ? (
               <img
-                src="VAH_20241202_232229_0000_page-0001.jpg"
+                src={ adminImage }
                 alt="Admin"
                 className="w-8 h-8 rounded-full"
               />
